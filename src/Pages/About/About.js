c109b0bc4dced83ac44f5654f01b28bea3fee3f9
hsmello <60452857+hsmello@users.noreply.github.com>
2020-07-03 14:00:29 +0100

    about

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import './About.css'
 
 export default function About() {
     return (
-        <div className="">
-            ABOUT
+        <div className="about_page">
+            <div className="about_title bg_shadow bg_common">
+                ABOUT
+            </div>
+
+            <div className="about_body">
+
+                <div className="ab_first_block">
+                    <div className="ab_first_block_bg bg_shadow bg_common">
+
+                    </div>
+                    <div className="ab_first_block_front front_shadow front_common">
+                        What is it?
+                    </div>
+
+                </div>
+
+                <div className="second_leg">
+
+                    <div className="ab_second_block">
+                        <div className="ab_second_block_bg bg_shadow bg_common">
+
+                        </div>
+                        <div className="ab_second_block_front front_shadow front_common">
+                            Source
+                        </div>
+
+                    </div>
+
+                    <div className="ab_third_block">
+                        <div className="ab_third_block_bg bg_shadow bg_common">
+
+                        </div>
+                        <div className="ab_third_block_front front_shadow front_common">
+                            Who am I?
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
         </div>
     )
 }
